d3c0432c74a880e997c35fc4a8438fef90e31f46
Bellah Ellam <bella.ellam@student.moringaschool.com>
2023-03-28 20:54:35 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+
+function submitData(name, email) {
+    const url = "http://localhost:3000/users";
+    const config = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({ name, email })
+    };
+    
+    return fetch(url, config)
+      .then(response => response.json())
+      .then(data => {
+        const id = data.id;
+        document.body.innerHTML += `<p>User ID: ${id}</p>`;
+      })
+      .catch(error => {
+        document.body.innerHTML += `<p>Error Message: ${error.message}</p>`;
+      });
+  }
